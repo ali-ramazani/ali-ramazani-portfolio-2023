634bc40b9ba5cb6ce42bd2e593753ec6c5be531b
ramazanim <ramazanim@berea.edu>
2023-12-14 17:36:34 -0500

    fixed next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
       },
-}
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push(
+        "@mongodb-js/zstd",
+        "snappy",
+        "mongodb-client-encryption"
+      );
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
